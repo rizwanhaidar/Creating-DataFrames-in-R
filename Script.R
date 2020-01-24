d9a073591479e42a49bbcdd8e4d3e8d6392d82c0
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-24 22:52:45 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -63,3 +63,19 @@ my_df <- mtcars[1:10,]
 
 # Construct list with these different elements:
 my_list <-list(my_vector,my_matrix,my_df)
+
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Adapt list() call to give the components names
+my_list <- list( vec=my_vector, mat=my_matrix, df=my_df)
+
+
+# Print out my_list
+my_list
