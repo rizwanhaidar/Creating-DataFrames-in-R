0adb0665a891995090a976feb783229db990368c
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-24 22:28:58 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -8,3 +8,11 @@ rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
 
 # Create a data frame from the vectors
 planets_df <- data.frame(name, type, diameter,rotation, rings)
+
+# The planets_df data frame from the previous exercise is pre-loaded
+
+# Print out diameter of Mercury (row 1, column 3)
+planets_df[1,3]
+
+# Print out data for Mars (entire fourth row)
+planets_df[4, ]
