ca35319fdd2bc6f6c4538f7fedf4e6bff02581e6
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-24 23:01:08 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -79,3 +79,19 @@ my_list <- list( vec=my_vector, mat=my_matrix, df=my_df)
 
 # Print out my_list
 my_list
+
+# The variables mov, act and rev are available
+
+# Finish the code to build shining_list
+shining_list <- list(moviename = mov, actors=act, reviews=rev)
+
+
+# shining_list is already pre-loaded in the workspace
+
+# Print out the vector representing the actors
+
+shining_list$actors
+# Print the second element of the vector representing the actors
+shining_list$actors[2]
+
+
