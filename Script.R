17199a5119d2f3a9b9f5f752825a203013dbd005
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-24 22:43:24 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -16,3 +16,50 @@ planets_df[1,3]
 
 # Print out data for Mars (entire fourth row)
 planets_df[4, ]
+
+# The planets_df data frame from the previous exercise is pre-loaded
+
+# Select first 5 values of diameter column
+planets_df[1:5, "diameter"]
+
+# planets_df is pre-loaded in your workspace
+
+# Select the rings variable from planets_df
+rings_vector <- planets_df$rings
+  
+# Print out rings_vector
+rings_vector
+
+# planets_df and rings_vector are pre-loaded in your workspace
+
+# Adapt the code to select all columns for planets with rings
+planets_df[rings_vector,]
+
+# planets_df is pre-loaded in your workspace
+
+# Select planets with diameter < 1
+subset(planets_df, diameter<1)
+
+# Play around with the order function in the console
+ a <- c(100, 10, 1000)
+ order(a)
+
+# planets_df is pre-loaded in your workspace
+
+# Use order() to create positions
+positions <-  order(planets_df$diameter)
+planets_df[positions, ]
+# Use positions to sort planets_df
+order(positions)
+
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Construct list with these different elements:
+my_list <-list(my_vector,my_matrix,my_df)
